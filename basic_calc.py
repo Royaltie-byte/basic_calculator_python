f73333b249e7e29decabd23c60b7ce20d11bd11c
Royaltie-byte <allan.kihiu@students.jkuat.ac.ke>
2025-03-15 21:29:54 +0300

    updated <basic_calc.py> .Fixed bugs and added new features.

--- basic_calc.py
+++ basic_calc.py
@@ -2,24 +2,39 @@ print("Enter two numbers that you want to perform an operation on :\n")
 num1=float(input("First number :"))
 num2=float(input("Second number :"))
 
-operator=input("Enter the operation that you wish to perform on the two numbers(+,-,*,/) :")
+
+#checking the validity of the operator so as to proceed.
+while True:
+    operator=input("Enter the operation that you wish to perform on the two numbers(+,-,*,/) :")
+    if operator in ('+','-','*','/'):
+        break
+    else:
+        print("Invalid operator!!!.Enter a valid  operator ")
+
+
+
 if operator=='+':
     result=num1+num2
-   
+
 elif operator== '-':
-    result=num1+num2
+    result=num1-num2
     
-   
+
 elif operator == '*':
     result=num1*num2
     
-   
-elif operator == '/':
-    result = num1/num2
-    
-else:
-    print("You entered a wrong operator, please enter the correct operator! ")
 
+elif operator == '/':
+    if num2 !=0:
+        result=num1/num2
+    else:
+        result="Error !!!Division by zero is not allowed."
 print(f"Result of {num1} {operator} {num2} = {result}")
+
+  
+
+
+
+
 
 
